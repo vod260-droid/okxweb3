a2474b8d633ff8bf707d4a6e29d1dbe4163ca8ff
vod260-droid <vod260@gmail.com>
2025-09-13 15:30:09 +0800

    Update [...path].js

--- api/okxweb3/[...path].js
+++ api/okxweb3/[...path].js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch';
 export default async function handler(req, res) {
   try {
     // 去掉 /api/okxweb3 前缀
+    res.status(200).json({ url: req.url });
     const prefix = '/api/okxweb3';
     let path = req.url || '/';
     if (path.startsWith(prefix)) {
